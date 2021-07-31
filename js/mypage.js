26fb432342985287a24c2f200f6a3a29f715e94f
hjlee0820 <umtt0820@gmail.com>
2021-07-31 21:55:29 +0900

    마이페이지 스크립트 수정
    체크박스 전체선택 기능

--- js/mypage.js
+++ js/mypage.js
@@ -1,31 +1,9 @@
 'use strict';
 
-const $c1 = document.getElementById('c1');
-const $c_arr = document.querySelectorAll('input[type=checkbox]');
-
-//전체 선택했을시
-$c1.addEventListener('click',e=>{
-  console.log('1');
-  //체크한거면
-  if(e.hasAttribute('checked')){
-    console.log('2');
-    //체크안된 체크박스들 체크
-    Array.from($c_arr).forEach(ele=>{
-      console.log('3');
-      if(!ele.hasAttribute('checked')){
-        ele.toggleAttribute('checked');
-      }
-    });
-  }
-  //체크 푼거면
-  if(!e.hasAttribute('checked')){
-    console.log('4');
-    //체크된 체크박스들 체크해제
-    Array.from($c_arr).forEach(ele=>{
-      console.log('5');
-      if(ele.hasAttribute('checked')){
-        ele.toggleAttribute('checked');
-      }
-    });
-  }
+//체크박스 전체선택
+bcategory_all.addEventListener('click',()=>{
+  const checkboxes = document.querySelectorAll('input[type=checkbox]');
+  checkboxes.forEach(checkbox=>{
+    checkbox.checked = bcategory_all.checked
+  });
 });
