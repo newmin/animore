3c51dcb65b0a66960c771ef91053bda3c628402c
newmin <82501822+newmin@users.noreply.github.com>
2021-08-05 17:32:34 +0900

    link server

---
+++ animore/src/main/java/com/proj/animore/AnimoreApplication.java
@@ -0,0 +1,13 @@
+package com.proj.animore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimoreApplication.class, args);
+	}
+
+}
---
+++ animore/src/test/java/com/proj/animore/AnimoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.animore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
