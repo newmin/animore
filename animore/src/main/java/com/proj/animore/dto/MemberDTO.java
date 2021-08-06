0d1584baa1ed0cf6bc7ec41a9f7e2f91e3194df1
newmin <82501822+newmin@users.noreply.github.com>
2021-08-06 17:32:06 +0900

    create MemberDTO

---
+++ animore/src/main/java/com/proj/animore/dto/MemberDTO.java
@@ -0,0 +1,19 @@
+package com.proj.animore.dto;
+
+import java.util.Date;
+
+import com.proj.animore.form.Gender;
+
+public class MemberDTO {
+
+  private String id;
+  private String pw;
+  private String pw2;
+  // TODO private 프로필사진타입 image;
+  private String name;
+  private Date birth;
+  private Gender gender;
+  private String tel;
+  private String email;
+  private String address;
+}
