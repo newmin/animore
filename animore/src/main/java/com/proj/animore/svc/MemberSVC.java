fe1ad08f822d365663f52bfaf7395574e76cdf5e
newmin <82501822+newmin@users.noreply.github.com>
2021-08-06 17:31:43 +0900

    create MemberSVC

---
+++ animore/src/main/java/com/proj/animore/svc/MemberSVC.java
@@ -0,0 +1,33 @@
+package com.proj.animore.svc;
+
+import java.util.List;
+
+import com.proj.animore.dto.MemberDTO;
+
+public interface MemberSVC {
+
+	//회원가입
+	void joinMember(MemberDTO memberDTO);
+	
+	//내정보 조회
+	MemberDTO findMemberById(String id);
+	
+	//내정보 수정
+	MemberDTO modifyMember(String id, MemberDTO memberDTO);
+	
+	//회원탈퇴
+	void deleteMember(String id);
+	
+	//회원전체목록(관리자)
+	List<MemberDTO> list();
+	
+	//로그인
+	MemberDTO findByIdPw(String id, String pw);
+	
+	//아이디찾기
+	String findId(String name, String email);
+	
+	//비밀번호 찾기
+	String findPw(String id, String name, String email);
+	
+}
---
+++ animore/src/main/java/com/proj/animore/svc/MemberSVCImpl.java
@@ -0,0 +1,63 @@
+package com.proj.animore.svc;
+
+import java.util.List;
+
+import com.proj.animore.dao.MemberDAO;
+import com.proj.animore.dto.MemberDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MemberSVCImpl implements MemberSVC {
+	
+	private final MemberDAO memberDAO;
+
+	//TODO 회원가입후 메인페이지로? 등록된 정보 보여주는 페이지로?
+	@Override
+	public void joinMember(MemberDTO memberDTO) {
+		memberDAO.joinMember(memberDTO);
+		//등록된정보 보여주는거라면 return값 memberDTO로 내정보보기메소드 실행
+	}
+
+	@Override
+	public MemberDTO findMemberById(String id) {
+		return memberDAO.findMemberById(id);
+	}
+
+	@Override
+	public MemberDTO modifyMember(String id, MemberDTO memberDTO) {
+		memberDAO.modifyMember(id, memberDTO);
+		return memberDAO.findMemberById(id);
+	}
+
+	@Override
+	public void deleteMember(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<MemberDTO> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MemberDTO findByIdPw(String id, String pw) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findId(String name, String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findPw(String id, String name, String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
