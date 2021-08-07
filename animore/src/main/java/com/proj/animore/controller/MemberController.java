a4c0527db8918f88661a637c20f2a0d031415227
hjlee0820 <umtt0820@gmail.com>
2021-08-07 18:48:52 +0900

    MemberController 회원유형선택-회원가입폼 까지 매핑 완료
    일반회원, 특수회원

--- animore/src/main/java/com/proj/animore/controller/MemberController.java
+++ animore/src/main/java/com/proj/animore/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.proj.animore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,11 +11,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member")
 public class MemberController {
 
+	/**
+	 * 회원가입유형선택
+	 * @return
+	 */
 	@GetMapping("/join")
-	public String joinForm() {
-		return "member/joinForm";
+	public String joinSelectMtype() {
+		return "member/joinSelectMtype";
+	}
+
+	/**
+	 * 회원가입폼
+	 * @param mtype
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/join/{mtype}")
+	public String joinForm(
+			@PathVariable("mtype") String mtype, Model model) {
+		
+		//회원유형 선택화면에서 회원유형값 받아서 조인폼으로 넘겨야됨.
+		//조인폼에서 받은 회원유형 값으로 보여줄 항목, 안보여줄 항목 선택되도록
+		
+		if(mtype.equals("일반"))	mtype="N";
+		if(mtype.equals("특수"))	mtype="S";
+		
+		model.addAttribute("mtype", mtype);
+		return "/member/joinForm";
 	}
 	
+
 	@PostMapping("/join")
 	public String join() {
 //		TODO DTO받아서 SVC실행
