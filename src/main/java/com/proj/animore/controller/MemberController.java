a4eeb1bde5e58fa2a0061e93b8604c3baaa8a33c
hjlee0820 <83224283+hjlee0820@users.noreply.github.com>
2021-08-18 08:47:58 +0900

    MemberController

--- src/main/java/com/proj/animore/controller/MemberController.java
+++ src/main/java/com/proj/animore/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.proj.animore.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
