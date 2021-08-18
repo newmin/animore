b76fb78d504577d769856abe0d5340c42c5e2957
hjlee0820 <umtt0820@gmail.com>
2021-08-18 22:46:44 +0900

    로그인 상태로 회원유형선택, 회원가입양식 페이지 요청시 메인페이지로 이동하도록 함

--- src/main/java/com/proj/animore/controller/MemberController.java
+++ src/main/java/com/proj/animore/controller/MemberController.java
@@ -1,10 +1,10 @@
 package com.proj.animore.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,69 +27,77 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/member")
 public class MemberController {
 
-   private final MemberSVC memberSVC;
-   
-   /**
-    * 회원가입유형선택
-    * @return
-    */
-   @GetMapping("/join")
-   public String joinSelectMtype() {
-      return "member/joinSelectMtype";
-   }
+	private final MemberSVC memberSVC;
 
-   /**
-    * 회원가입양식
-    * @param mtype
-    * @param model
-    * @return
-    */
-   @GetMapping("/join/{mtype}")
-   public String joinForm(@ModelAttribute JoinMemberForm joinMemberForm,
-         HttpServletRequest request,
-         @PathVariable("mtype") String mtype, Model model) {
-      
-      //회원유형 선택화면에서 회원유형값 받아서 조인폼으로 넘겨야됨.
-      //조인폼에서 받은 회원유형 값으로 보여줄 항목, 안보여줄 항목 선택되도록
-      
-      if(mtype.equals("normal"))   mtype="N";
-      if(mtype.equals("special"))   mtype="S";
-      
-      
-      model.addAttribute("mtype", mtype);
-      return "member/joinForm";
-   }
-   
+	/**
+	 * 회원가입유형선택
+	 * 
+	 * @return
+	 */
+	@GetMapping("/join")
+	public String joinSelectMtype(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+
+		// 로그인한 상태(세션있음)로 회원가입유형선택 페이지 요청시 메인페이지로 보냄
+		if (session != null)	return "redirect:/";
+		// 로그인 안한 상태(세션없음)로 회원가입유형선택 페이지 요청해야 페이지로 이동
+		return "member/joinSelectMtype";
+	}
+
+	/**
+	 * 회원가입양식
+	 * 
+	 * @param mtype
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/join/{mtype}")
+	public String joinForm(@ModelAttribute JoinMemberForm joinMemberForm, HttpServletRequest request,
+			@PathVariable("mtype") String mtype, Model model) {
+		HttpSession session = request.getSession(false);
+
+		// 로그인한 상태(세션있음)로 회원가입양식 페이지 요청시 메인페이지로 보냄
+		if (session != null)	return "redirect:/";
+
+		// 회원유형 선택화면에서 회원유형값{mtype} 받아서 조인폼으로 넘겨주게 됨.
+		// joinForm에서 th:if문을 사용하여 회원유형값이 N이면 일반회원 가입양식,
+		// 회원유형값이 S이면 특수회원 가입양식이 보여짐.
+		if (mtype.equals("normal"))		mtype = "N";
+		if (mtype.equals("special"))	mtype = "S";
+
+		model.addAttribute("mtype", mtype);
+		return "member/joinForm";
+	}
+
 //   @PostMapping("/join/N")
 //   public String join1(MemberDTO memberDTO) {
 //	      memberSVC.joinMember(memberDTO);
 //	      return "redirect:/";
 //	   }
-   
-   @PostMapping("/join/N")
-   public String join(@Valid
-		   			@ModelAttribute JoinMemberForm joinMemberForm, 
-		   				MemberDTO memberDTO ,
-		   				BindingResult bindingResult) {
-	   if(!joinMemberForm.getPw().equals(joinMemberForm.getPw2())) {
-		   bindingResult.rejectValue("pw2","pw2","비밀번호가 일치하지 않습니다.");
-	   }
-	   if(bindingResult.hasErrors()) {
-			log.info("errors={}",bindingResult);
+
+	@PostMapping("/join/N")
+	public String join(@Valid @ModelAttribute JoinMemberForm joinMemberForm, MemberDTO memberDTO,
+			BindingResult bindingResult) {
+		if (!joinMemberForm.getPw().equals(joinMemberForm.getPw2())) {
+			bindingResult.rejectValue("pw2", "pw2", "비밀번호가 일치하지 않습니다.");
+		}
+		if (bindingResult.hasErrors()) {
+			log.info("errors={}", bindingResult);
 			return "member/joinForm";
 		}
-	   memberSVC.joinMember(memberDTO);
-	   return "redirect:/";
-   }
-   //TODO 전문가 회원가입
-   @PostMapping("/join/S")
-   public String join2(MemberDTO memberDTO, BusinessDTO businessDTO) {
-      memberSVC.joinMember(memberDTO, businessDTO);
-      return "redirect:/";
-   }
-   
-   @GetMapping("/{id}")
-   public String mypage() {
-      return "mypage/mypage";
-   }
+		memberSVC.joinMember(memberDTO);
+		return "redirect:/";
+	}
+
+	// TODO 전문가 회원가입
+	@PostMapping("/join/S")
+	public String join2(MemberDTO memberDTO, BusinessDTO businessDTO) {
+		memberSVC.joinMember(memberDTO, businessDTO);
+		return "redirect:/";
+	}
+
+	@GetMapping("/{id}")
+	public String mypage() {
+		return "mypage/mypage";
+	}
 }
