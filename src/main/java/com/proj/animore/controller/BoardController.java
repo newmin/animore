983b8b391543b82cf4e8e330be081203d89704f7
juhee0614 <85140495+juhee0614@users.noreply.github.com>
2021-08-18 21:34:00 +0900

    게시판 카테고리별 출력

--- src/main/java/com/proj/animore/controller/BoardController.java
+++ src/main/java/com/proj/animore/controller/BoardController.java
@@ -2,8 +2,11 @@ package com.proj.animore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.proj.animore.form.BoardForm;
 import com.proj.animore.svc.BoardSVC;
 
 import lombok.RequiredArgsConstructor;
@@ -16,8 +19,14 @@ import lombok.extern.slf4j.Slf4j;
 public class BoardController {
 	private final BoardSVC boardSVC;
 	
-	@GetMapping("")
-	public String boardList() {
+	@GetMapping("/{bcategory}")
+	public String boardList(@ModelAttribute BoardForm boardForm,
+							@PathVariable String bcategory) {
+	     if(bcategory.equals("Q"))   bcategory="Q";
+	     if(bcategory.equals("M"))   bcategory="M";
+	     if(bcategory.equals("F"))   bcategory="F";
+	     if(bcategory.equals("P"))   bcategory="P";
+		
 		return "board/board";
 	}
 
