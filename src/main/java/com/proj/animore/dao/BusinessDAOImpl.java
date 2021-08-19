533aa650a58cb5dbd31f20e4a0e7f54a34043a53
mypc <mypc@DESKTOP-G808FHE>
2021-08-19 16:07:00 +0900

    비즈니스DAO구현

--- src/main/java/com/proj/animore/dao/BusinessDAOImpl.java
+++ src/main/java/com/proj/animore/dao/BusinessDAOImpl.java
@@ -2,6 +2,7 @@ package com.proj.animore.dao;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 public class BusinessDAOImpl implements BusinessDAO {
 
 	private final JdbcTemplate jdbcTemplate;
-	
+	//업체등록
 	@Override
 	public void joinBusi(BusinessDTO businessDTO) {
 		StringBuffer sql = new StringBuffer();
@@ -32,17 +33,38 @@ public class BusinessDAOImpl implements BusinessDAO {
 												businessDTO.getOpenhours());
 		
 	}
-
+	//업체찾기
 	@Override
 	public BusinessDTO findBusiByBbnum(String bnum) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuffer sql = new StringBuffer();
+		//sql문
+		sql.append("select bunm,bname,");
+		sql.append("from business");
+		sql.append("where bnum = ? ");
+		BusinessDTO businessDTO = jdbcTemplate.queryForObject(sql.toString(),
+				new BeanPropertyRowMapper<>(BusinessDTO.class),
+				bnum);
+		
+		
+		return businessDTO;
 	}
 
 	@Override
 	public BusinessDTO modifyBusi(String bnum, BusinessDTO businessDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sql = new StringBuilder();
+		sql.append("update business ");
+		sql.append("   set bname = ?, ");
+		sql.append("       baddress = ?, ");
+		sql.append("       btel = ? ");
+		sql.append(" where bnm = ? ");
+		
+		jdbcTemplate.update(
+				sql.toString(), 
+				businessDTO.getBname(),
+				businessDTO.getBaddress(),
+				businessDTO.getBtel()
+				);
+		return findBusiByBbnum(bnum);
 	}
 
 	@Override
@@ -55,6 +77,21 @@ public class BusinessDAOImpl implements BusinessDAO {
 	public List<BusinessDTO> list() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	//업체추가
+	public void addBusi(BusinessDTO businessDTO) {
+		StringBuffer sql = new StringBuffer();
+		
+		sql.append("insert into business(bnum, bbnum, id, bname, baddress, btel, openhours) values(business_bnum_seq.nextval,?,?,?,?,?,?) ");
+		
+		jdbcTemplate.update(sql.toString(),
+												businessDTO.getBbnum(),
+												businessDTO.getId(),
+												businessDTO.getBname(),
+												businessDTO.getBaddress(),
+												businessDTO.getBtel(),
+												businessDTO.getOpenhours());
+		
 	}
 	
 	
