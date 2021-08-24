e0c81af7f59d63427d1d8f3630c64f89136619a4
newmin <82501822+newmin@users.noreply.github.com>
2021-08-24 19:02:48 +0900

    메인페이지에서 게시글조회 구현

--- src/main/java/com/proj/animore/controller/HomeController.java
+++ src/main/java/com/proj/animore/controller/HomeController.java
@@ -1,15 +1,31 @@
 package com.proj.animore.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.proj.animore.dto.BoardReqDTO;
+import com.proj.animore.svc.BoardSVC;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class HomeController {
 
+	private final BoardSVC boardSVC;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		
+		 String bcategory = "P";
+		 List<BoardReqDTO> list = boardSVC.list(bcategory);
+     model.addAttribute("post",list);
+     
 		return "index";
 	}
 }
