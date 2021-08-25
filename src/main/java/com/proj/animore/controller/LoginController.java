dd745993a199fd400cc7243800ad8ec409f55809
juhee0614 <85140495+juhee0614@users.noreply.github.com>
2021-08-25 16:33:28 +0900

    login controller 주석제거

--- src/main/java/com/proj/animore/controller/LoginController.java
+++ src/main/java/com/proj/animore/controller/LoginController.java
@@ -38,11 +38,8 @@ public class LoginController {
 		HttpSession session = request.getSession(false);
 
 		// 로그인한 상태(세션있음)로 로그인양식 페이지 요청시 메인페이지로 보냄
-<<<<<<< HEAD
 		if (session != null)	return "redirect:/";
-=======
-		//if (session != null)	return "redirect:/";		
->>>>>>> board
+
 		
 		return "/member/login";
 	}
