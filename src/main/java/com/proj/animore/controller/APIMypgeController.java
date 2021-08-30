a7f9877769e054375ddf6911b49a48ebb6bab108
newmin <82501822+newmin@users.noreply.github.com>
2021-08-30 14:57:41 +0900

    Create APIMypageController

---
+++ src/main/java/com/proj/animore/controller/APIMypgeController.java
@@ -0,0 +1,46 @@
+package com.proj.animore.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proj.animore.dto.ReviewReq;
+import com.proj.animore.form.LoginMember;
+import com.proj.animore.form.Result;
+import com.proj.animore.svc.ReviewSVC;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/mypage")
+@RequiredArgsConstructor
+public class APIMypgeController {
+
+	private final ReviewSVC reviewSVC;
+	
+	@GetMapping("/review")
+	public Result myReview(HttpServletRequest request) {
+		Result result;
+		HttpSession session = request.getSession(false);
+		if(session == null) {
+			result = new Result("01","로그인후 내정보 페이지를 이용하실 수 있습니다.",null);
+			return result;
+		}
+		
+		LoginMember loginMember = (LoginMember)session.getAttribute("loginMember");
+		String id = loginMember.getId();
+		
+		List<ReviewReq> list = reviewSVC.myReview(id);
+		result = new Result("00","성공",list);
+		return result;
+	}
+	
+	
+}
