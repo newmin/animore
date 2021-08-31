feab45e2166643c234890f792b25103df21920fc
hjlee0820 <umtt0820@gmail.com>
2021-08-31 15:11:28 +0900

    내가쓴댓글,회원탈퇴 ajax 사용 완료

--- src/main/java/com/proj/animore/controller/MyPageController.java
+++ src/main/java/com/proj/animore/controller/MyPageController.java
@@ -83,25 +83,69 @@ public class MyPageController {
 	
 	
 	
+//	@GetMapping("/mypageReply")
+//	public String mypageReply(
+//			HttpServletRequest request,
+//			Model model) {
+//		
+//		HttpSession session = request.getSession(false);
+//		LoginMember loginMember = (LoginMember)session.getAttribute("loginMember");
+//		log.info("loginMember:{}",loginMember);
+//		
+//		List<MypageReplyRes> list = mypageSVC.mypageReply(loginMember.getId());
+//		
+//		model.addAttribute("mypagereply", list);
+//		
+////		list.forEach(ele->{
+////			log.info("ele:{}",ele);
+////		});
+//		return "mypage/mypageReply";
+//	}
+	
+	
+	@ResponseBody
 	@GetMapping("/mypageReply")
-	public String mypageReply(
-			HttpServletRequest request,
-			Model model) {
-		
+	public Result mypageReply(HttpServletRequest request) {
+	
 		HttpSession session = request.getSession(false);
 		LoginMember loginMember = (LoginMember)session.getAttribute("loginMember");
 		log.info("loginMember:{}",loginMember);
 		
 		List<MypageReplyRes> list = mypageSVC.mypageReply(loginMember.getId());
 		
-		model.addAttribute("mypagereply", list);
+		StringBuffer html = new StringBuffer();
+			
+			html.append("<h3 class='mypage_content_title'>내가 쓴 댓글</h3>");
+			html.append("<hr>");
+			html.append("<div class='mypage_content'>");
+			html.append("  <table class='mypagereply__'> ");
+			html.append("    <tr>");
+			html.append("      <th class='mypagereply__title1'>번호</th>");
+			html.append("      <th class='mypagereply__title2'>댓글내용</th>");
+			html.append("      <th class='mypagereply__title3'>작성일</th>");
+			html.append("      <th class='mypagereply__title4'>좋아요</th>");
+			html.append("       <!-- <th class='mypagereply__title5'></th> -->");
+			html.append("    </tr>");
+		list.forEach(rec->{
+			html.append("    <tr>");
+			html.append("      <td class='mypagereply__text1'>"+rec.getRnum()+"</td>");
+			html.append("      <td class='mypagereply__text2'><a href='/board/post/"+rec.getBnum()+"'>"+rec.getRcontent()+"</a></td>");
+			html.append("      <td class='mypagereply__text3'>"+rec.getRcdate()+"</td>");
+			html.append("      <td class='mypagereply__text4'>"+rec.getBgood()+"</td>");
+			html.append("      <!-- <td th:text='5번째칸' class='mypagereply__text'></td> -->");
+			html.append("    </tr>");
+		});
+			html.append("  </table>");
+			html.append("</div>");
 		
-//		list.forEach(ele->{
-//			log.info("ele:{}",ele);
-//		});
-		return "mypage/mypageReply";
+		Result result;
+		result = new Result("00","OK",html);
+		
+	//	list.forEach(ele->{
+	//		log.info("ele:{}",ele);
+	//	});
+		return result;
 	}
-	
 	
 	@ResponseBody
 	@GetMapping("/mypageDel")
@@ -110,17 +154,17 @@ public class MyPageController {
 		Result result;
 		
 		StringBuffer html = new StringBuffer();
-		html.append("<h3 class=\"mypage_content_title\">회원탈퇴</h3>");
+		html.append("<h3 class='mypage_content_title'>회원탈퇴</h3>");
 		html.append("<hr>");
-		html.append("<div class=\"mypage_content\">");
-		html.append("  <form action=\"/mypage/mypageDel\" th:method=\"delete\" class=\"findId\">");
-		html.append("    <h1 class=\"findId__title\"></h1>");
-		html.append("    <!-- <p class=\"login__errormsg\" th:errors=\"*{global}\"></p> -->");
-		html.append("    <div class=\"findId__form\">");
-		html.append("      <span class=\"findId__text\">비밀번호 확인</span>");
-		html.append("      <input class=\"findId__input\" type=\"text\" name=\"pw\">");
+		html.append("<div class='mypage_content'>");
+		html.append("  <form action='/mypage/mypageDel' th:method='delete' class='findId'>");
+		html.append("    <h1 class='findId__title'></h1>");
+		html.append("    <!-- <p class='login__errormsg' th:errors='*{global}'></p> -->");
+		html.append("    <div class='findId__form'>");
+		html.append("      <span class='findId__text'>비밀번호 확인</span>");
+		html.append("      <input class='findId__input' type='text' name='pw'>");
 		html.append("    </div>");
-		html.append("    <button class=\"findId__btn\" type=\"submit\">회원탈퇴</button>");
+		html.append("    <button class='findId__btn' type='submit'>회원탈퇴</button>");
 		html.append("  </form>");
 		html.append("</div>");
 		
