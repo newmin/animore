a2766d789a48c2a5a060edb54394e39a760a5c2e
mickey0301 <soska0301@naver.com>
2021-09-03 15:36:01 +0900

    개인정보수정

--- src/main/java/com/proj/animore/controller/MyPageController.java
+++ src/main/java/com/proj/animore/controller/MyPageController.java
@@ -37,94 +37,113 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @SessionAttributes("id")
 public class MyPageController {
-	private final FavoriteSVC favoriteSVC;
-	private final MemberSVC memberSVC;
-	//즐겨찾기 목록
-	@GetMapping("/mypageFavorites")
-	public String mypage(HttpServletRequest request,
-			Model model) {
-				
-		HttpSession session = request.getSession(false);
- 		LoginMember loginMember = (LoginMember)session.getAttribute("loginMember");
- 		String id = loginMember.getId();
- 		log.info(id);
+   private final FavoriteSVC favoriteSVC;
+   private final MemberSVC memberSVC;
+   //즐겨찾기 목록
+   @GetMapping("/mypageFavorites")
+   public String mypage(HttpServletRequest request,
+         Model model) {
+            
+      HttpSession session = request.getSession(false);
+       LoginMember loginMember = (LoginMember)session.getAttribute("loginMember");
+       String id = loginMember.getId();
+       log.info(id);
 
- 		List<FavoriteReq> favoritelist = favoriteSVC.favoriteList(id);
+       List<FavoriteReq> favoritelist = favoriteSVC.favoriteList(id);
 
- 		model.addAttribute("Favorite",favoritelist);
+       model.addAttribute("Favorite",favoritelist);
 
-		return "mypage/mypageFavorites";
-	}
+      return "mypage/mypageFavorites";
+   }
 
-	//회원탈퇴처리
-	@DeleteMapping("/mypageDel")
-	public String mypageDel(
-			@RequestParam String pw,
-			HttpServletRequest request,
-			Model model) {
-		log.info("회원탈퇴");
-		
-		Map<String, String> errors = new HashMap<>();
-		
-		if(pw == null || pw.trim().length() == 0) {
-			errors.put("pw", "비밀번호를 입력하세요");
-			model.addAttribute("errors", errors);
-//			return "mypage/memberOutForm";
-			return null;
-		}
-		
-		HttpSession session = request.getSession(false);
-		if(session == null) return "redirect:/login";
-		
-		LoginMember loginMember = (LoginMember)session.getAttribute("loginMember");
-		//회원존재유무 확인
-		if(memberSVC.isMemember(loginMember.getId(), pw)) {
-			//탈퇴
-			memberSVC.outMember(loginMember.getId(), pw);
-		}else {
-			errors.put("global", "비밀번호가 잘못되었습니다.");
-			model.addAttribute("errors", errors);
-		}
-		
-		if(!errors.isEmpty()) {
-//			return "mypage/memberOutForm";
-			return null;
-		}
-		
-		session.invalidate();
-		
-		return "redirect:/";
-	}
-	//개인정보 수정양식
-	@GetMapping("/mypage")
+   //회원탈퇴처리
+   @DeleteMapping("/mypageDel")
+   public String mypageDel(
+         @RequestParam String pw,
+         HttpServletRequest request,
+         Model model) {
+      log.info("회원탈퇴");
+      
+      Map<String, String> errors = new HashMap<>();
+      
+      if(pw == null || pw.trim().length() == 0) {
+         errors.put("pw", "비밀번호를 입력하세요");
+         model.addAttribute("errors", errors);
+//         return "mypage/memberOutForm";
+         return null;
+      }
+      
+      HttpSession session = request.getSession(false);
+      if(session == null) return "redirect:/login";
+      
+      LoginMember loginMember = (LoginMember)session.getAttribute("loginMember");
+      //회원존재유무 확인
+      if(memberSVC.isMemember(loginMember.getId(), pw)) {
+         //탈퇴
+         memberSVC.outMember(loginMember.getId(), pw);
+      }else {
+         errors.put("global", "비밀번호가 잘못되었습니다.");
+         model.addAttribute("errors", errors);
+      }
+      
+      if(!errors.isEmpty()) {
+//         return "mypage/memberOutForm";
+         return null;
+      }
+      
+      session.invalidate();
+      
+      return "redirect:/";
+   }
+   //개인정보 수정양식
+   @GetMapping("/mypage")
 public String modifyMember(HttpServletRequest request,
-		Model model) {
-		log.info("회원양식 호출");
-		HttpSession session  = request.getSession(false);
-		LoginMember loginMember = 
-				(LoginMember)session.getAttribute("loginMember");
-		
-		if(loginMember == null)return "redirect:/login";
-		
-		//회원정보 가져오기
-		
-		MemberDTO memberDTO = memberSVC.findMemberById(loginMember.getId());
-		ModifyForm modifyForm = new ModifyForm();
-		BeanUtils.copyProperties(memberDTO, modifyForm);
-		
-		model.addAttribute("modifyForm",modifyForm);
-	
-	
-	
-	return "redirect:/mypageModify";
+      Model model) {
+      log.info("회원양식 호출");
+      HttpSession session  = request.getSession(false);
+      LoginMember loginMember = 
+            (LoginMember)session.getAttribute("loginMember");
+      
+      if(loginMember == null)return "redirect:/login";
+      
+      //회원정보 가져오기
+      
+      MemberDTO memberDTO = memberSVC.findMemberById(loginMember.getId());
+      ModifyForm modifyForm = new ModifyForm();
+      BeanUtils.copyProperties(memberDTO, modifyForm);
+      
+      model.addAttribute("modifyForm",modifyForm);
+   
+   
+   
+   return "redirect:/mypageModify";
 }
-	@PatchMapping("/mypage")
-	public String modifyMember(@Valid @ModelAttribute ModifyForm modifyForm,
-			BindingResult bindingResult,
-			HttpServletRequest request) {
-		
-		
-		return "redirect:/mypage/mypageModify";
-	}
-	
+   
+   //개인정보 수정처리
+   @PatchMapping("/mypage")
+   public String modifyMember(@Valid @ModelAttribute ModifyForm modifyForm,
+         BindingResult bindingResult,
+         HttpServletRequest request) {
+         log.info("회원수정처리 호출됨");
+         HttpSession session = request.getSession(false);
+         LoginMember loginMember =
+               (LoginMember)session.getAttribute("loginMember");
+         log.info("회원 수정 처리:{}"+loginMember.toString());
+         //세션이 없으면 로그인 페이지로 이동
+         if(loginMember == null) return "redirect:/login";
+         //비밀번호를 잘못 입력 했을경우
+         if(!memberSVC.isMemember(loginMember.getId(), modifyForm.getPw())) {
+            bindingResult.rejectValue("pw", "error.member.modifyForm", "비밀번호가 잘못입력되었습니다.");
+         }
+         if(bindingResult.hasErrors()) {
+            log.info("errors={}",bindingResult);
+            return "mypage/modifyForm";
+         }
+         MemberDTO mdto = new MemberDTO();
+         memberSVC.modifyMember(loginMember.getId(), mdto);
+         log.info("=={},{}",loginMember.getId(), mdto);
+
+         return "redirect:/mypage/mypageModify";
+   }
+   
 }
