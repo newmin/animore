8ca5c955c91b643c540f830aff3f5dad12cfd2d6
juhee0614 <85140495+juhee0614@users.noreply.github.com>
2021-09-03 15:04:41 +0900

    즐겨찾기svc

--- src/main/java/com/proj/animore/svc/FavoriteSVCImpl.java
+++ src/main/java/com/proj/animore/svc/FavoriteSVCImpl.java
@@ -9,35 +9,36 @@ import com.proj.animore.dto.FavoriteReq;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-@Service
-public class FavoriteSVCImpl implements FavoriteSVC {
-	
-	private final FavoriteDAO favoriteDAO;
 
-	//즐겨찾기 등록
-	@Override
-	public void addFavorite(Integer bnum, String id) {
-		favoriteDAO.addFavorite(bnum,id);	
-	}
+	@RequiredArgsConstructor
+	@Service
+	public class FavoriteSVCImpl implements FavoriteSVC {
+		
+		private final FavoriteDAO favoriteDAO;
 
-	//즐겨찾기 삭제
-	@Override
-	public void deleteFavorite(Integer bnum, String id) {
-		favoriteDAO.deleteFavorite(bnum,id);
-	}
+		//즐겨찾기 등록
+		@Override
+		public void addFavorite(Integer bnum, String id) {
+			favoriteDAO.addFavorite(bnum,id);	
+		}
 
-	//즐겨찾기 목록
-	@Override
-	public List<FavoriteReq>favoriteList(String id) {
-		return favoriteDAO.favoriteList(id);
-	}
-	
-	//즐겨찾기 조회
-	@Override
-//	public int isFavorite(Integer bnum, String id) {
-	public FavoriteReq isFavorite(Integer bnum, String id) {
-		return favoriteDAO.isFavorite(bnum, id);
-	}
+		//즐겨찾기 삭제
+		@Override
+		public void deleteFavorite(Integer bnum, String id) {
+			favoriteDAO.deleteFavorite(bnum,id);
+		}
+
+		//즐겨찾기 목록
+		@Override
+		public List<FavoriteReq>favoriteList(String id) {
+			return favoriteDAO.favoriteList(id);
+		}
+		
+		//즐겨찾기 조회
+		@Override
+//		public int isFavorite(Integer bnum, String id) {
+		public FavoriteReq isFavorite(Integer bnum, String id) {
+			return favoriteDAO.isFavorite(bnum, id);
+		}
 
 }
