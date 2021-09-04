7acf062454c6e9ce683ce631d419bdd1926e274f
hjlee0820 <umtt0820@gmail.com>
2021-09-04 20:16:54 +0900

    병원정보 공공데이터 서버에 저장 기능 구현

---
+++ src/main/java/com/proj/animore/openapi/Hospital.java
@@ -0,0 +1,15 @@
+package com.proj.animore.openapi;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class Hospital {
+	String gugun;
+	String address;
+	String entId;
+	Date appDate;
+	String tel;
+	String title;
+}
--- src/main/java/com/proj/animore/openapi/HospitalController.java
+++ src/main/java/com/proj/animore/openapi/HospitalController.java
@@ -1,6 +1,7 @@
 package com.proj.animore.openapi;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +17,20 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api")
 public class HospitalController {
 
-	private final HospitalOpenAPI hospital;
+	private final HospitalOpenAPI2 hospital;
 	
 	@ResponseBody
 	@GetMapping("/hospital")
 	public String hospital(@ModelAttribute HospitalParam hospitalParam) throws IOException {
-		String hospitalXML = hospital.getHospital(hospitalParam);
-		return hospitalXML;
-	}	
+//		String hospitalXML = hospital.getHospital(hospitalParam);
+
+//		HospitalOpenAPI hoa = new HospitalOpenAPI();
+		HospitalParam hp = new HospitalParam();
+		hp.setNumOfRows(hospitalParam.getNumOfRows());
+		hp.setPageNo(hospitalParam.getPageNo());
+		
+		List<Hospital> hList = hospital.getHospital(hp);
+		
+		return hList.toString();
+	}
 }
