39084e42814194902e259eeca5136ca0bc3afff9
hjlee0820 <umtt0820@gmail.com>
2021-09-05 17:46:14 +0900

    업체상세정보 페이지의 마커이벤트 제거

--- src/main/resources/static/js/mapAPI3.js
+++ src/main/resources/static/js/mapAPI3.js
@@ -12,7 +12,7 @@ var map = new kakao.maps.Map(mapContainer, mapOption);
 var geocoder = new kakao.maps.services.Geocoder();
 //주소목록
 
-let $preEle = '';
+// let $preEle = '';
 
 
 
@@ -31,36 +31,30 @@ let $preEle = '';
       // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
       map.setCenter(coords);
 
-      // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-      // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-      var iwContent = '<div style="width:150px;text-align:center;padding:6px 0;">'+
-                        '<a href="/inquire/'+$busi.bnum+'">'+$busi.bname+'</a>'+
-                      '</div>'
+      // // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
+      // // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+      // var iwContent = '<div style="width:150px;text-align:center;padding:6px 0;">'+
+      //                   '<a href="/inquire/'+$busi.bnum+'">'+$busi.bname+'</a>'+
+      //                 '</div>'
 
-          ,iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+      //     ,iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
-      // 인포윈도우를 생성합니다
-      var infowindow = new kakao.maps.InfoWindow({
-        content : iwContent,
-        removable : iwRemoveable
-      });
+      // // 인포윈도우를 생성합니다
+      // var infowindow = new kakao.maps.InfoWindow({
+      //   content : iwContent,
+      //   removable : iwRemoveable
+      // });
 
-      // 마커에 클릭이벤트를 등록합니다
-      kakao.maps.event.addListener(marker, 'click', function() {
-        // 기존에 열린 인포윈도우 닫기
-        infowindow.close(map, marker);
-        // 마커 위에 인포윈도우를 표시합니다
-        infowindow.open(map, marker);
-      });
+      // // 마커에 클릭이벤트를 등록합니다
+      // kakao.maps.event.addListener(marker, 'click', function() {
+      //   // 기존에 열린 인포윈도우 닫기
+      //   infowindow.close(map, marker);
+      //   // 마커 위에 인포윈도우를 표시합니다
+      //   infowindow.open(map, marker);
+      // });
 
-      kakao.maps.event.addListener(map, 'click', function(){
-        infowindow.close(map, marker);
-      });
-
-      // if(!!$preEle) $preEle.style.fontSize = "1em";
-      // e.target.style.fontSize = "1.3em";
-      //   $preEle = e.target;
-
-
+      // kakao.maps.event.addListener(map, 'click', function(){
+      //   infowindow.close(map, marker);
+      // });
     };
   });
