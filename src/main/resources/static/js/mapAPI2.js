3b880e70e41c958ae3d115830d0a99f8a042c5fd
hjlee0820 <umtt0820@gmail.com>
2021-09-05 21:54:48 +0900

    지도 중심좌표기준 주소정보 표시 중간저장

--- src/main/resources/static/js/mapAPI2.js
+++ src/main/resources/static/js/mapAPI2.js
@@ -10,7 +10,7 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
-//주소목록
+
 
 // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
 if (navigator.geolocation) {
@@ -42,6 +42,40 @@ function displayMarker(locPosition, message) {
   // 지도 중심좌표를 접속위치로 변경합니다
   map.setCenter(locPosition);      
 }    
+
+
+// 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+
+// 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'idle', function() {
+  searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+});
+
+function searchAddrFromCoords(coords, callback) {
+  // 좌표로 행정동 주소 정보를 요청합니다
+  geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);         
+}
+
+function searchDetailAddrFromCoords(coords, callback) {
+  // 좌표로 법정동 상세 주소 정보를 요청합니다
+  geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+}
+
+// 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+function displayCenterInfo(result, status) {
+  if (status === kakao.maps.services.Status.OK) {
+      var infoDiv = document.getElementById('centerAddr');
+
+      for(var i = 0; i < result.length; i++) {
+          // 행정동의 region_type 값은 'H' 이므로
+          if (result[i].region_type === 'H') {
+              infoDiv.innerHTML = result[i].address_name;
+              break;
+          }
+      }
+  }    
+}
 
 
 var selectedMarker = null;  // 클릭한 마커를 담을 변수
@@ -89,10 +123,11 @@ $busiList.forEach( (rec, index) => {
       });
 
 
-      const $listATag = document.querySelector(`a[href='/inquire/${$busiList[index].bnum}']`).parentElement.parentElement;
+      const $listATag = document.querySelector(`a[href='/inquire/${$busiList[index].bnum}']`);
       $listATag.addEventListener('mouseover', function(){
         // 업체목록에 마우스오버 이벤트를 등록합니다
         makeInfoWindow($busiList,index);
+        map.setCenter(coords);
         // 업체목록에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
         infowindow.open(map, marker);
       });
