a5bdb851fa9bd848f30f9e41b4665855536a6c30
newmin <82501822+newmin@users.noreply.github.com>
2021-09-06 09:45:31 +0900

    BusinessDAOImpl 오탈자 수정

--- src/main/java/com/proj/animore/dao/BusinessDAOImpl.java
+++ src/main/java/com/proj/animore/dao/BusinessDAOImpl.java
@@ -85,7 +85,7 @@ public class BusinessDAOImpl implements BusinessDAO {
 		sql.append("                                from review ");
 		sql.append("                                group by bnum) r  ");
 		sql.append("where b.bnum=c.bnum  ");
-		sql.append("  and b.bnum=r.bnum(+)  ");
+		sql.append("  and b.bnum=r.bnum(+)	  ");
 		sql.append("  and "+bcategory+" = 'Y'  ");
 		
 		List<BusinessLoadDTO> list = jdbcTemplate.query(sql.toString(),
@@ -99,15 +99,15 @@ public class BusinessDAOImpl implements BusinessDAO {
 	@Override
 	public List<BusinessLoadDTO> busiListForMember(String bcategory, String id) {
 		StringBuffer sql = new StringBuffer();
-		sql.append("select b.BNUM,b.BBNUM,b.BNAME,b.BADDRESS,b.BTEL,b.NIGHTCARE,b.RAREANI,b.VISITCARE,b.HOLIDAYOPEN,b.DENTAL, r.bscore  ");
-		sql.append("from business b left join favorite f on b.bnum=f.bnum, bcategory c, (select bnum, round(avg(rscore),2) bscore ");
+		sql.append("select b.BNUM,b.BBNUM,b.BNAME,b.BADDRESS,b.BTEL,b.NIGHTCARE,b.RAREANI,b.VISITCARE,b.HOLIDAYOPEN,b.DENTAL, r.bscore ");
+		sql.append("  from business b left join favorite f on b.bnum=f.bnum, bcategory c, (select bnum, round(avg(rscore),2) bscore ");
 		sql.append("                                from review ");
-		sql.append("                                group by bnum) r  ");
-		sql.append("where b.bnum=c.bnum  ");
-		sql.append("  and b.bnum=r.bnum  ");
-		sql.append("  and "+bcategory+" = 'Y'  ");
-		sql.append("  and f.id = ?  ");
-		sql.append("order by fdate desc null last ");
+		sql.append("                                group by bnum) r ");
+		sql.append(" where b.bnum=c.bnum ");
+		sql.append("   and b.bnum=r.bnum ");
+		sql.append("   and "+bcategory+" = 'Y' ");
+		sql.append("   and f.id = ? ");
+		sql.append(" order by fdate desc nulls last ");
 		
 		List<BusinessLoadDTO> list = jdbcTemplate.query(sql.toString(),
 				   new BeanPropertyRowMapper<>(BusinessLoadDTO.class),id);
