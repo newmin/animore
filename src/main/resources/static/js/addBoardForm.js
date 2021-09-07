43a09b1671864cd095fa14c6c707f2e4c58d6872
juhee0614 <85140495+juhee0614@users.noreply.github.com>
2021-09-07 19:05:03 +0900

    게시판 사진첨부 css 수정

--- src/main/resources/static/js/addBoardForm.js
+++ src/main/resources/static/js/addBoardForm.js
@@ -5,3 +5,14 @@
  $escBtn.addEventListener('click',e=>{
 	location.href=`/board/Q`;
 })
+
+function uploadChange(file) {
+	var el = file.parentNode.parentNode.getElementsByTagName("*");
+	for (var i = 0; i < el.length; i++) {
+	  var node = el[i];
+	  if (node.className == "file-text") {
+		node.innerHTML = file.value;
+		break;
+	  }
+	}
+}
