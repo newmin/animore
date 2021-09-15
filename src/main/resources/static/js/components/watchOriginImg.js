d4faacd5a257a922995b7ed4a429330e8c6d0a3b
newmin <rbals7587@naver.com>
2021-09-16 01:55:28 +0900

    사진 클릭시 새창에서 조회

---
+++ src/main/resources/static/js/components/watchOriginImg.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+ //프로필사진(sm)
+ const $profile__sm = document.querySelectorAll('.profile__sm');
+ $profile__sm.forEach(ele=>ele.addEventListener('click',e=>{
+	image_popup(e.target.src);
+}))
+//리뷰 첨부사진
+ const $revire__img = document.querySelectorAll('.revire__img');
+ $revire__img.forEach(ele=>ele.addEventListener('click',e=>{
+	image_popup(e.target.src);
+}))
+
+
+function image_popup(url) {
+ const imgObj = new Image();
+ imgObj.src = url;
+ imageWin = window.open("", "profile_popup", "width="+imgObj.width+"px, height="+imgObj.height+"px"); 
+ imageWin.document.write("<html>"); 
+ //imageWin.document.write("<style>img{width:100%;height:100%;}</style>"); 
+ imageWin.document.write("</html><body style='margin:0'>"); 
+ imageWin.document.write("<a href=javascript:window.close()><img src='"+imgObj.src+"' border=0></a>"); 
+ imageWin.document.write("</body><html>"); 
+ imageWin.document.title = imgObj.src;
+}
