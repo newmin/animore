29b09aa13ef131b7facfab233f493c71882aec12
newmin <rbals7587@naver.com>
2021-10-01 11:53:16 +0900

    일반회원 가입시 비밀번호 유효성 확인 오류현상 처리

--- src/main/resources/static/js/joinForm.js
+++ src/main/resources/static/js/joinForm.js
@@ -9,7 +9,7 @@ $homeBtn.addEventListener('click',e=>{
 });
 
  const $bhospital = document.getElementById('bhospital');
- $bhospital.addEventListener('click',()=>{
+ $bhospital?.addEventListener('click',()=>{
    h_tags.classList.toggle('active');
    Array.from(document.querySelectorAll('#h_tags input')).forEach(ele=>{
  		ele.toggleAttribute('disabled');
